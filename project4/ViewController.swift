e4e0c103033254b3eb287e752eed9c5134bc8b4c
Dhany Basuki <dddhany+github@gmail.com>
2016-03-10 22:55:32 +0800

    choosing website

--- project4/ViewController.swift
+++ project4/ViewController.swift
@@ -20,14 +20,34 @@ class ViewController: UIViewController, WKNavigationDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .Plain, target: self, action: "openTapped")
+        
         let url = NSURL(string: "https://www.bloomberg.com")!
         webView.loadRequest(NSURLRequest(URL: url))
         webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    func openTapped() {
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .ActionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .Default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "bloomberg.com", style: .Default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        presentViewController(ac, animated: true, completion: nil)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        let url = NSURL(string: "https://"+action.title!)!
+        webView.loadRequest(NSURLRequest(URL: url))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+        title = webView.title
     }
 
 
